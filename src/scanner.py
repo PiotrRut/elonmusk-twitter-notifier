a20f02ec9dad9e59102619f1735290550a355566
PiotrRut <piotr.rutkowski@city.ac.uk>
2021-02-11 12:28:26 +0000

    Small update to exclude fetching retweets and mentions

--- src/scanner.py
+++ src/scanner.py
@@ -7,7 +7,7 @@ class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         keywords = ['stock', 'Stock', 'share', 'Share', '$', 'doge', 'Doge']
         # this solution will exclude replies and mentions and only return Elon's tweets
-        if any(tweet in status.text for tweet in keywords) and '@elonmusk' not in status.text:
+        if any(tweet in status.text for tweet in keywords) and status.user.id_str == '44196397':
             # send e-mail
             send_mail(f"Elon tweeted: {status.text}")
 
