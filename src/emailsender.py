be35fe1a4c3ec99e4444525bc528ae61731fc902
PiotrRut <piotr.rutkowski@city.ac.uk>
2021-02-20 11:12:15 +0000

    Send alert to 2+ email addresses

--- src/emailsender.py
+++ src/emailsender.py
@@ -4,14 +4,16 @@ from email.mime.text import MIMEText
 
 mail_from = os.getenv('EMAIL_FROM')
 mail_to = os.getenv('EMAIL_TO')
+mail_to2 = os.getenv('EMAIL_TO2')
 mail_pw = os.getenv('EMAIL_PW')
 
 
-# Sending an e-mail to myself whenever a tweet that meets the criteria is detected
+# Sending an e-mail to myself (and some friends) whenever a tweet that meets the criteria is detected
 def send_mail(text):
     msg = MIMEText(text)
+    mailing_list = [mail_to, mail_to2]
     msg['From'] = mail_from
-    msg['To'] = mail_to
+    msg['To'] = ', '.join(mailing_list)
     msg['Subject'] = 'New stonks tweet alert!'
 
     smtp_server_name = 'smtp.gmail.com'
